5815c8dfaceb9da822f1988d6c5dde9bf38dd9a4
Лёхич <alexsuvorov2506@gmail.com>
2025-10-01 23:16:18 +0300

    Update api_reader.py
    Add country names from API request

--- api_example/api_reader.py
+++ api_example/api_reader.py
@@ -1,16 +1,24 @@
 import requests
-# import pandas as pd
+import pandas as pd
 
 API_URL = "https://restcountries.com/v3.1"
 
-items = []
+countries = []
 response = requests.get(
     API_URL + "/all?fields=name", 
     headers={"Content-Type": "application/json"},
 )
 if response.status_code == 200:
-    items = response.json()
-    print(items)
+    countries = response.json()
 else:
     msg = f"Response code is {response.status_code}"
     raise Exception(msg)
+
+country_names = []
+
+for country in countries:
+    country_names.append(country.get('name')['common'])
+
+print(country_names)
+
+df = pd.DataFrame(countries)
